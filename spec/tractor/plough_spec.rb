bae604debe55274ef4d8255b3db72d3304eed122
Hans Kreuger <luke.dolman@gmail.com>
2012-09-02 10:20:31 +0100

    more tests

--- spec/tractor/plough_spec.rb
+++ spec/tractor/plough_spec.rb
@@ -2,10 +2,13 @@ require 'spec_helper'
 
 module Tractor
     describe Plough do
-        it "can have sites" do
-        pending("TODO"){    
-            plough = Tractor::Plough.new('example')
-        }
+        let(:site) { double("Site").as_null_object }
+        let(:plough) { Tractor::Plough.new('example',[site]) }
+        it "has a name" do
+            plough.name.should == 'example'
+        end
+        it "has sites" do
+            plough.sites.should have(1).items
         end
     end
 end
