0b386db3cf2e3d1ff6bac7ccde17b74b2bf80f4f
Damacus <dan.m.webb@gmail.com>
2012-09-11 13:28:29 +0100

    refactored out some of the main class

---
+++ lib/tractor/server.rb
@@ -0,0 +1,29 @@
+class Server
+    attr_accessor :hostname
+    attr_accessor :username
+    attr_accessor :basedir
+    attr_accessor :ssh
+
+    def initialize(hostname, ssh, username=hostname, basedir='/var/www/vhosts')
+        @hostname = hostname
+        @username = username
+        @basedir = basedir
+        @ssh = ssh
+    end
+
+    def revision
+        cmd_result = @ssh.execute("svn info")
+        raise("site not found") unless cmd_result.is_a?(Numeric)
+    end
+
+    def update
+        begin
+            revision
+        rescue
+            @ssh.execute("svn checkout #{basedir}")
+        else
+            @ssh.execute("svn update #{basedir}")
+        end
+    end
+end
+
