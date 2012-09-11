6b242ba02e8c418fc3c8d272c96b1a17fdc69fd8
Damacus <dan.m.webb@gmail.com>
2012-09-11 13:28:29 +0100

    refactored out some of the main class

---
+++ lib/tractor/site.rb
@@ -0,0 +1,31 @@
+class Site
+    attr_accessor :name
+    attr_accessor :servers
+    attr_accessor :status
+    
+    def initialize(name='default', servers=Hash.new, status=false)
+        @name = name
+        @servers = servers
+        @status = status
+    end
+    def revision
+        revisions = servers.collect{ |server| server.revision }
+        if revisions.uniq.one?
+            revisions.first
+        else
+            raise "server revision mismatch #{revisions}"
+        end
+    end
+    def query
+        {   
+            :revision => revision,
+            :status => @status,
+            :servers => @servers.collect{ |server| server.hostname }
+        } 
+    end
+    def deployed?
+        @status == 'deployed' 
+    end
+end
+
+
